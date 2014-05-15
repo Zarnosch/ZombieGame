76f04703a1bf4bd85a6be3a531606f5f66afe3bf
Zarnosch <janoszwei@yahoo.de>
2014-05-15 12:42:29 +0200

    lol

--- src/FlagUmfeld.java
+++ src/FlagUmfeld.java
@@ -1,24 +1,29 @@
 
 import core.Vector;
+import core.ai.AiFlagInfo;
 import core.ai.AiMapInfo;
 import core.ai.AiPlayerInfo;
+import core.elements.Flag;
 
-public class FlagUmfeld {
+public class FlagUmfeld{
 	//Constants
 	private Vector longestDist = new Vector (Float.MAX_VALUE,Float.MAX_VALUE);
-	//Nearest Flag Vector
-	public Vector getNearestFlag(AiMapInfo map, AiPlayerInfo ownPlayer){
+	private Flag flagge1 = new Flag(new Vector(1000f, 1000f));
+	private AiFlagInfo psydoFlag = new AiFlagInfo(flagge1);
+	//Nearest Flag Vector (Half InsertionSort) "Wächter"-Element = longestDist Aufwnd: O(n) bzw O(2n) = 20 Vergleiche bei 10 Flaggen
+	public Vector[] getNearestFlag(AiMapInfo map, AiPlayerInfo ownPlayer){
 		Vector nextFlag = new Vector(0,0);
-		Vector tempFlag = longestDist;
+		Vector[] tempFlags = {longestDist, new Vector(0,0)};
 		for(int i = 0; i < map.getNumFlags(); i++){
 			if(!(map.getFlags()[i].isOwner(ownPlayer))){
 				nextFlag = map.getFlags()[i].getPosition().sub(ownPlayer.getPosition());
-				if(nextFlag.length() < tempFlag.length()){
-					tempFlag = nextFlag;
+				if(!(nextFlag.length() >= tempFlags[0].length())){
+					tempFlags[1] = tempFlags[0];
+					tempFlags[0] = nextFlag;
 				}
 			}
 		}
-		return tempFlag;
+		return tempFlags;
 	}
 
 	public int getConqueredFlags(AiMapInfo map, AiPlayerInfo ownPlayer){
@@ -29,5 +34,43 @@ public class FlagUmfeld {
 			}
 		}
 		return flagsOwned;
+	}
+
+	public Vector[] getFlagArray(AiMapInfo map){
+		Vector[] allFlags = new Vector[map.getNumFlags()];
+		for(int i = 0; i < map.getNumFlags(); i++){
+			allFlags[i] = map.getFlags()[i].getPosition();
+		}
+		return allFlags;
+	}
+	//InsertionSort O(n²/2) O(n) with already sorted Arrays, stable!
+	public AiFlagInfo[] sortAllFlags(AiMapInfo map , AiPlayerInfo ownPlayer){
+		AiFlagInfo[] flags = new AiFlagInfo[map.getNumFlags()];
+		
+		for(int k = 0; k < flags.length ; k++){
+			if(!(map.getFlags()[k].isOwner(ownPlayer))){
+				flags[k] = map.getFlags()[k];
+			}
+			else flags[k] = psydoFlag;
+		}
+		
+		Vector ownPlayer_ = ownPlayer.getPosition();
+		
+		for(int i = 1; i < flags.length; i++){
+			AiFlagInfo x = flags[i];
+			int j;				
+			for(j = i; j > 0 && flags[j-1].getPosition().sub(ownPlayer_).length() >= x.getPosition().sub(ownPlayer_).length() ;--j){
+				flags[j] = flags[j-1];
+			}
+			flags[j] = x;
+		}
+		return flags;
+	}
+	public Vector[] AiFlagInfoToVector(AiFlagInfo flags[]){
+		Vector[] tempVec = new Vector[flags.length];
+		for(int i = 0; i < tempVec.length; i++){
+			tempVec[i] = flags[i].getPosition();
+		}
+		return tempVec;
 	}
 }
--- src/King_Joffrey.java
+++ src/King_Joffrey.java
@@ -1,16 +1,18 @@
 import core.Vector;
+import core.ai.AiFlagInfo;
 import core.ai.AiMapInfo;
 import core.ai.AiPlayerInfo;
 import core.player.PlayerController;
 import core.map.*;
+import debug.DebugFlags;
 
 public class King_Joffrey extends PlayerController{
-private int stepsAlive = 1;
+	private int stepsAlive = 1;
 	@Override
 	public String getName() {
 		return "King Joffrey";
 	}
-	
+
 	@Override
 	public String getAuthor() {
 		return "Janos Zimmermann";
@@ -18,55 +20,86 @@ private int stepsAlive = 1;
 	// Think batman
 	@Override
 	public Vector think(AiMapInfo map, AiPlayerInfo ownPlayer) {
+		long time1 = System.nanoTime();
+		debugInfo.clearCircles();
 		//Steps alive
 		stepsAlive += 1;
 		//Died
 		boolean died = new Died().died(map, ownPlayer);
+		// Nearest 2 Zombie search
+		Vector[] nearestZombies = new ZombieUmfeld().getNearestZombie(map, ownPlayer);
 		// Own Position
 		Vector ownPosition = ownPlayer.getPosition();
-		// Next Flag search
 		int conqueredFlags = new FlagUmfeld().getConqueredFlags(map, ownPlayer);
-		Vector nearestFlag = new Vector(0,0);
-		if(conqueredFlags < map.getNumFlags()){
-			nearestFlag = new FlagUmfeld().getNearestFlag(map, ownPlayer);
+		// Next 2 Flags search
+		Vector[] nearestFlags = {Vector.ZERO().add(ownPosition), Vector.ZERO()};
+//		Vector[] allFlags = new FlagUmfeld().getFlagArray(map);
+		AiFlagInfo[] nearestFlags_ = new FlagUmfeld().sortAllFlags(map, ownPlayer); // Sort Flaggs
+		nearestFlags = new FlagUmfeld().AiFlagInfoToVector(nearestFlags_); //Parse Flaginfo to Array
+		if(conqueredFlags < map.getNumFlags() ){
+//			nearestFlags = new FlagUmfeld().getNearestFlag(map, ownPlayer); //Old
+			nearestFlags[0] = nearestFlags_[0].getPosition();               //New
+			nearestFlags[1] = nearestFlags_[1].getPosition();
+			nearestFlags[2] = nearestFlags_[2].getPosition();
+			if(map.getZombiesInRadius(nearestFlags[1], 15f).length -1 > map.getZombiesInRadius(nearestFlags[2], 15f).length ){
+				Vector temp = nearestFlags[2];
+				nearestFlags[1] = temp;
+			}
+			if(map.getZombiesInRadius(nearestFlags[0], 10f).length  > map.getZombiesInRadius(nearestFlags[1], 10f).length ){
+				Vector temp = nearestFlags[1];
+				nearestFlags[0] = temp;
+			}
 		}
-		else nearestFlag = Vector.ZERO().add(ownPosition);
+
 		// Set standart movement
-		Vector movement = (Vector.ZERO().add(nearestFlag));
-		// Nearest Zombie search
-		Vector[] nearestZombies = new ZombieUmfeld().getNearestZombie(map, ownPlayer);
-		// Posision of the CombieCluster
-//		Vector zombieCluster = new ZombieCluster().getZombieCluster(map, ownPlayer);
-//		System.out.println("ZombieCluster movement: " + movement);
-//		movement = (Vector.ZERO().sub((zombieCluster).div(7)).add(nearestFlag).mult(700));
+		Vector movement = (new Vector().ZERO().add(nearestFlags[0].sub(ownPosition)));
 		// Movement reaction to near Zombies
 		if((map.getZombiesInRadius(ownPosition, ownPlayer.getCurrentNoiseRadius()+3).length) > 0){
-			movement = (Vector.ZERO().sub(nearestZombies[0])).add((nearestFlag).div(3));
+			movement = (Vector.ZERO().sub(nearestZombies[0])).add((nearestFlags[0]).div(3));
 		}
 		// Movement if Zombies got triggered
-		if((map.getZombiesInRadius(ownPlayer.getPosition(), ownPlayer.getCurrentNoiseRadius()-1).length) > 0){
-			movement = (Vector.ZERO().sub(nearestZombies[0])).add((nearestFlag).div(4));
+		if((map.getZombiesInRadius(ownPlayer.getPosition(), ownPlayer.getCurrentNoiseRadius()).length) > 0){
+			movement = (Vector.ZERO().sub(nearestZombies[0])).add((nearestFlags[0]).div(5));
 		}
-//		if((map.getZombiesInRadius(ownPlayer.getPosition(), ownPlayer.getCurrentNoiseRadius()+3).length) > 1)
-//			movement = (nearestZombies[0].sub(nearestZombies[1]));
-//			movement = new Vector(movement.y,movement.x);
+		if((map.getZombiesInRadius(ownPlayer.getPosition(), ownPlayer.getCurrentNoiseRadius()+2).length) > 1){
+			movement = (nearestZombies[0].sub(nearestZombies[1]));
+			movement.normalize();
+			movement = new Vector(movement.y,-movement.x); // Normale 1
+			if(movement.mult(1.2f).sub(nearestZombies[0]).length() < movement.mult(-1.2f).sub(nearestZombies[0]).length()){
+				movement = new Vector(-movement.y,movement.x); // Normale 2
+			}
+		}
+		if(map.getNumFlags() == conqueredFlags && map.getZombiesInRadius(ownPosition, ownPlayer.getCurrentNoiseRadius()+3).length > 0){
+			movement = (Vector.ZERO().sub(nearestZombies[0].x, -nearestZombies[0].y));
+		}
+	
+
+		//Bottle-throw reaction to near Zombies
+		if(map.getPassedSteps() == 25){
+			throwBottle((nearestFlags[0].mult(1.5f)).add(ownPosition));
+		}
+		if(((stepsAlive * ownPlayer.getNumBottles() > 600) && 
+				(map.getZombiesInRadius(ownPosition, ownPlayer.getCurrentNoiseRadius()+2).length) > 0) || 
+				((map.getZombiesInRadius(ownPosition, ownPlayer.getCurrentNoiseRadius()+2).length) > 1) &&
+				(stepsAlive * ownPlayer.getNumBottles() > 500 )){
+			throwBottle((nearestZombies[0].mult(1.8f)).add(ownPosition));
+		}
 		if(died){
 			stepsAlive = 1;
 		}
-		
-		// Bottle-throw reaction to near Zombies (Deaths)
-		if(map.getPassedSteps() == 1){
-			throwBottle((nearestFlag).mult(0.1f));
-		}
-		if(((stepsAlive * ownPlayer.getNumBottles() > 500) && 
-				(map.getZombiesInRadius(ownPosition, ownPlayer.getCurrentNoiseRadius()+2).length) > 0) || 
-				((map.getZombiesInRadius(ownPosition, ownPlayer.getCurrentNoiseRadius()+2).length) > 1) &&
-				(stepsAlive * ownPlayer.getNumBottles() > 400 )){
-			throwBottle((nearestZombies[0].mult(1.3f)).add(ownPosition.mult(1f)));
-		}
+//		if(map.getPassedSteps() <= 7){
+//			movement = new Vector(0,1);
+//		}
+		//Debug an NanoTime Measurement
+		debugInfo.addDebugCircle(ownPosition, ownPlayer.getCurrentNoiseRadius());
+		debugInfo.getDebugCircles().add(new DebugCircle(nearestFlags[0], 5));
+		debugInfo.getDebugCircles().add(new DebugCircle(nearestFlags[1], 5));
+//		System.out.println(System.nanoTime()- time1);
 		movement.normalize();
-		movement.mult(10);
-		movement.normalize();
+		if(ownPlayer.getNoiseRadius(movement) < nearestZombies[0].euclideanDistance(ownPosition)){
+			movement.div(5);
+		}
+		//test
 		return movement;
 	}
 }
